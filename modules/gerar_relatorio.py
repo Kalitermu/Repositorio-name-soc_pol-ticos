7aea25b60c7e73a9ed18d450fac1ea679d9835d0
Ariel Jose Luiz <joseluizariel@gmail.com>
2026-03-10 18:31:45 +0000

    relatorio pdf com grafico

--- modules/gerar_relatorio.py
+++ modules/gerar_relatorio.py
@@ -1,6 +1,18 @@
 from fpdf import FPDF
+import matplotlib.pyplot as plt
 
 def gerar_pdf(cidade, score, gasto_total):
+
+    # criar gráfico
+    labels = ["Score de risco"]
+    valores = [score]
+
+    plt.bar(labels, valores)
+    plt.title("Indice de risco fiscal")
+
+    grafico="grafico_risco.png"
+    plt.savefig(grafico)
+    plt.close()
 
     pdf = FPDF()
     pdf.add_page()
@@ -12,6 +24,8 @@ def gerar_pdf(cidade, score, gasto_total):
     pdf.cell(200,10,txt=f"Cidade: {cidade}",ln=True)
     pdf.cell(200,10,txt=f"Score de risco: {score}",ln=True)
     pdf.cell(200,10,txt=f"Gasto total: {gasto_total}",ln=True)
+
+    pdf.image(grafico, x=10, y=80, w=100)
 
     arquivo="relatorio_fiscal.pdf"
     pdf.output(arquivo)
