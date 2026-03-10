bec301cf05134b402edb15c53e8ac25560163524
Ariel Jose Luiz <joseluizariel@gmail.com>
2026-03-10 21:13:38 +0000

    fix tesouro api rreo

--- tesouro_api.py
+++ tesouro_api.py
@@ -2,15 +2,29 @@ import requests
 import pandas as pd
 
 def buscar_dados_municipio(codigo_ibge="3550308"):
-    url = f"https://apidatalake.tesouro.gov.br/ords/siconfi/tt/rreo?codigo_ibge={codigo_ibge}"
+
+    url = "https://apidatalake.tesouro.gov.br/ords/siconfi/tt/rreo"
+
+    params = {
+        "codigo_ibge": codigo_ibge,
+        "limit": 2000
+    }
 
     try:
-        r = requests.get(url, timeout=30)
-        dados = r.json()["items"]
+
+        r = requests.get(url, params=params, timeout=30)
+
+        if r.status_code != 200:
+            return pd.DataFrame()
+
+        dados = r.json().get("items", [])
+
+        if not dados:
+            return pd.DataFrame()
 
         df = pd.DataFrame(dados)
 
         return df
 
-    except Exception as e:
+    except Exception:
         return pd.DataFrame()
