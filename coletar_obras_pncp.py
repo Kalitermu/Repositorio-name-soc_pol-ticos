5878c35ffa16cf140170ad7e9a5a8685af53a0ab
Kalitermu <joseluizariel@gmail.com>
2026-03-16 12:26:15 -0300

    Update coletar_obras_pncp.py

--- coletar_obras_pncp.py
+++ coletar_obras_pncp.py
@@ -5,26 +5,30 @@ def buscar_obras():
 
     contratos = []
 
-    for pagina in range(1, 50):
+    for pagina in range(1,20):
 
         url = f"https://pncp.gov.br/api/consulta/v1/contratos?pagina={pagina}"
 
         try:
+
             r = requests.get(url, timeout=30)
             dados = r.json()
 
-            if not isinstance(dados, list) or len(dados) == 0:
-                break
+            if not isinstance(dados, list):
+                continue
 
             for item in dados:
+
                 contratos.append({
+
                     "cidade": item.get("orgaoEntidade", {}).get("municipioNome"),
-                    "empresa": item.get("fornecedor", ""),
-                    "valor_inicial": item.get("valorInicial", 0),
-                    "valor_total": item.get("valorGlobal", 0),
-                    "data_inicio": item.get("dataVigenciaInicial", ""),
-                    "data_fim": item.get("dataVigenciaFinal", ""),
-                    "descricao": item.get("objeto", "")
+
+                    "descricao": item.get("objeto",""),
+
+                    "empresa": item.get("fornecedor",""),
+
+                    "valor_total": item.get("valorGlobal",0)
+
                 })
 
         except:
